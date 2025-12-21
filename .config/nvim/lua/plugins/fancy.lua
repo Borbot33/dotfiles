8d463e1ff1662ac69cd8ed2ae281d50a8a3b2b65
Borbot33 <58076174+Borbot33@users.noreply.github.com>
2025-12-21 11:19:18 +0100

    finally update the neovim config

---
+++ .config/nvim/lua/plugins/fancy.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		'romgrk/barbar.nvim',
+		dependencies = {
+			'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+			'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+		},
+		init = function() vim.g.barbar_auto_setup = false end,
+		opts = {},
+		version = '^1.0.0', -- optional: only update when a new 1.x version is released
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional, but recommended
+		},
+		config = function()
+			vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>')
+		end,
+		lazy = false,	
+	},
+	{
+		'akinsho/bufferline.nvim', 
+		version = "*", 
+		dependencies = 'nvim-tree/nvim-web-devicons',
+		config = function()
+			require("bufferline").setup()
+		end,
+	},
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+			require('lualine').setup()
+		end,
+	}
+}
---
+++ .config/nvim/lua/plugins/highlighting.lua
@@ -0,0 +1,5 @@
+return {
+	{
+		"l-zeuch/yagpdb.vim",
+	}
+}
